549752c54946a30a8d3a587460922874d38f1bb1
Joshua Hughes <kivhift@gmail.com>
2014-03-27 14:43:38 -0400

    pu.scons.utils: Add zip_from_config().
    The new function does the same thing as tar_gz_from_config() using a ZIP
    file as archive format since this format is handier when deploying things
    to Windows users.  Some of their shared code was generalized and tar_*()
    was refactored a bit as a result.

--- src/pu/scons/utils.py
+++ src/pu/scons/utils.py
@@ -1,10 +1,11 @@
 #
-# Copyright (c) 2010-2012 Joshua Hughes <kivhift@gmail.com>
+# Copyright (c) 2010-2014 Joshua Hughes <kivhift@gmail.com>
 #
 import contextlib
 import os
 import tarfile
 import tempfile
+import zipfile
 
 from pu.utils import import_code, is_a_string
 
@@ -40,6 +41,18 @@ def fix_trailing_whitespace(target, source, env):
 
 decrlf = fix_trailing_whitespace
 
+def _add_base(base, name):
+    return '/'.join([base, name])
+
+def _load_archive_config(src):
+    with open(src.path) as f:
+        cfg = import_code(f, os.path.splitext(src.name)[0])
+
+    if not hasattr(cfg, 'archive_config'):
+        raise AttributeError('No archive_config in {}.'.format(src.path))
+
+    return cfg
+
 def tar_gz_from_config(target, source, env):
     '''
     This function produces a gzipped tar archive target from the config
@@ -62,24 +75,16 @@ def tar_gz_from_config(target, source, env):
         x/e
     '''
     t = str(target[0])
-    s = source[0]
     if t.endswith('.tgz'):
         base = t[:-4]
     elif t.endswith('.tar.gz'):
         base = t[:-7]
     else:
         base = os.path.splitext(t)[0]
-
     base = os.path.basename(base)
+    add_base = lambda name: _add_base(base, name)
 
-    def add_base(name):
-        return '/'.join([base, name])
-
-    with open(s.path) as srcf:
-        cfg = import_code(srcf, os.path.splitext(s.name)[0])
-
-    if not hasattr(cfg, 'archive_config'):
-        raise AttributeError('No archive_config in %s.' % s.path)
+    cfg = _load_archive_config(source[0])
 
     with contextlib.closing(tarfile.open(name = t, mode = 'w:gz')) as tf:
         for f in cfg.archive_config:
@@ -87,6 +92,38 @@ def tar_gz_from_config(target, source, env):
                 tf.add(f, add_base(f))
             else:
                 tf.add(f[0], add_base(f[1]))
+
+def zip_from_config(target, source, env):
+    """Produce a zip file `target` from the config file in `source`.
+
+    See the documentation for :func:`tar_gz_from_config` for the format of
+    the config file.
+    """
+
+    t = str(target[0])
+    base = os.path.basename(os.path.splitext(t)[0])
+    add_base = lambda name: _add_base(base, name)
+    join = lambda x, y: '/'.join([x,y])
+
+    cfg = _load_archive_config(source[0])
+
+    with zipfile.ZipFile(t, 'w', zipfile.ZIP_DEFLATED) as z:
+        for f in cfg.archive_config:
+            if is_a_string(f):
+                fn, an = f, add_base(f)
+            else:
+                fn, an = f[0], add_base(f[1])
+            z.write(fn, an)
+            # It would be nice if you could add directories recursively.  Oh
+            # well...
+            if os.path.isdir(fn):
+                toadd = os.listdir(fn)
+                while toadd:
+                    entry = toadd.pop(0)
+                    fne = join(fn, entry)
+                    z.write(fne, join(an, entry))
+                    if os.path.isdir(fne):
+                        toadd.extend(join(entry, x) for x in os.listdir(fne))
 
 def always_build(dependency, target, prev_ni):
     """Decide to always build.
